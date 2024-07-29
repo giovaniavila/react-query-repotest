a587561b0a6d5f8b6cce4f671dae0141bf0c809c
giovaniavila <giovanicarvalhoavila@hotmail.com>
2024-07-29 15:33:00 -0300

    feat: add db with json server

--- core-project/src/App.tsx
+++ core-project/src/App.tsx
@@ -1,13 +1,21 @@
 import { ChakraProvider } from "@chakra-ui/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
 import theme from "./theme";
 import MainRoutes from "./routes";
 
+const queryclient = new QueryClient();
+
 function App() {
   return (
-    <ChakraProvider theme={theme}>
-      <MainRoutes />
-    </ChakraProvider>
+    <QueryClientProvider client={queryclient}>
+      <ChakraProvider theme={theme}>
+        <MainRoutes />
+        <ReactQueryDevtools intialIsOpen={false} />
+      </ChakraProvider>
+    </QueryClientProvider>
   );
 }
 
-export default App; 
+export default App;
