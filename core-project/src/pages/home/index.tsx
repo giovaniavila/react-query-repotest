07fbe0ba264d1e081c2a64f10f9c5651abd95a83
giovaniavila <giovanicarvalhoavila@hotmail.com>
2024-07-31 14:24:56 -0300

    feat: add form page

--- core-project/src/pages/home/index.tsx
+++ core-project/src/pages/home/index.tsx
@@ -1,15 +1,31 @@
-import { Box, Grid } from "@chakra-ui/react"
-import Sidebar from "../../components/sidebar"
+import {
+  Grid,
+  FormControl,
+  FormLabel,
+  Input,
+  Stack,
+} from "@chakra-ui/react";
+import Sidebar from "../../components/sidebar";
+import { Button } from "../../components/Button";
+
 
 const Home = () => {
   return (
     <Grid gridTemplateColumns=".2fr 1fr" h="100vh">
-        <Sidebar/>
-        <Box>
-            Conteúdo restante 
-        </Box>
+      <Sidebar />
+      <Stack spacing={2} w="50vw" placeSelf="start center" mt="2rem">
+        <FormControl>
+          <FormLabel>Name</FormLabel>
+          <Input type="text" backgroundColor="gray.50" />
+        </FormControl>
+        <FormControl>
+          <FormLabel>Email</FormLabel>
+          <Input type="email" backgroundColor="gray.50" />
+        </FormControl>
+        <Button type="submit" text="enviar"/>
+      </Stack>
     </Grid>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
